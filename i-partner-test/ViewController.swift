c4c205963d3ccf3cd0cdfea3e855644dd3c28fc8
Maria Paderina <mariapaderina79@gmail.com>
2019-05-19 23:12:15 +0300

    Initial Commit

---
+++ i-partner-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-partner-test
+//
+//  Created by Maria Paderina on 5/19/19.
+//  Copyright © 2019 Maria Paderina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i-partner-testTests/i_partner_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  i_partner_testTests.swift
+//  i-partner-testTests
+//
+//  Created by Maria Paderina on 5/19/19.
+//  Copyright © 2019 Maria Paderina. All rights reserved.
+//
+
+import XCTest
+@testable import i_partner_test
+
+class i_partner_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
